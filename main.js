8d9d624bff3ccff61a76fda676471a62a664b743
JordanBlane <jordan26265071@live.com>
2019-11-01 09:57:01 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+function solution(target) {
+    var coins = [1, 2, 5, 10, 20, 50, 100, 200];
+    var table = new Uint32Array(target + 1);
+    table[0] = 1;
+    for (var i = 0; i < coins.length; i++) {
+      for (var j = coins[i]; j <= target; j++) {
+        table[j]+= table[j - coins[i]];
+      }
+    }
+    return table[target];
+}
+console.log(solution(200))
